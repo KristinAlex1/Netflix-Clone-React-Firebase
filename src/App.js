67df6e0ebdc7aed8a6827d84230f41c218bd9738
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-07 13:26:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+    <h1>
+      Lets start the build
+    </h1>
+      
+    </div>
+  );
+}
+
+export default App;
